7576f6144734ec241b7db1fcb0abbab32e2c3bf8
Seung_Woo <114336975+Dev-SSW@users.noreply.github.com>
2022-10-04 14:11:17 +0900

    Update and rename 단어의 개수.java to STEP6/단어의 개수.java

--- STEP6/단어의 개수.java
+++ STEP6/단어의 개수.java
@@ -21,3 +21,23 @@ public class Main {
 		System.out.println(st.countTokens()); //토큰 개수 세기	
 	}
 }
+//방법 2 (편법)
+import java.util.*;
+public class Main {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		String s = sc.nextLine();
+		int count = 0;
+		for (int i = 0; i<s.length(); i++) {
+			if(s.charAt(i) == ' ') {
+				count++;
+			}
+		}
+		if (s.charAt(0) == ' ') {
+			count--;
+		}
+		if (s.charAt(s.length()-1) == ' ')
+			count--;
+		System.out.println(count+1);
+	}
+}
